beca7f55901dd16f694ae1b51c65a224748ddb5b
Oronno Akter Usha <ushaoronno1@gmail.com>
2025-01-06 23:31:50 +0600

    feat: add text animation

--- Part 1/script.js
+++ Part 1/script.js
@@ -12,11 +12,26 @@
 //   delay: 1,
 // });
 
-gsap.to("#box1", {
-  x: 1200,
+// gsap.to("#box1", {
+//   x: 1200,
+//   duration: 2,
+//   delay: 1,
+//   rotate: 360,
+//   backgroundColor: "blue",
+//   borderRadius: "50%",
+// });
+
+// ----- Text animation -----
+
+// gsap.from("h1", {
+//   color: "red",
+//   duration: 2,
+//   delay: 1,
+// });
+gsap.from("h1", {
+  opacity: 0,
   duration: 2,
+  y: 30,
   delay: 1,
-  rotate: 360,
-  backgroundColor: "blue",
-  borderRadius: "50%",
+  stagger: 0.5,
 });
