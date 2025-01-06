aeecfa0d62e0df2faf21dc65cc8575e6a8a156e4
Oronno Akter Usha <ushaoronno1@gmail.com>
2025-01-06 23:12:38 +0600

    feat: add box animation

---
+++ Part 1/script.js
@@ -0,0 +1,22 @@
+// ----- Box animation -----
+
+// gsap.to("#box1", {
+//   x: 1200,
+//   duration: 2,
+//   delay: 1,
+// });
+
+// gsap.from("#box2", {
+//   x: 1200,
+//   duration: 2,
+//   delay: 1,
+// });
+
+gsap.to("#box1", {
+  x: 1200,
+  duration: 2,
+  delay: 1,
+  rotate: 360,
+  backgroundColor: "blue",
+  borderRadius: "50%",
+});
