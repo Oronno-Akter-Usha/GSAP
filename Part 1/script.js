9e10b557cafae5d5ad0bf3cd1965a476fb794ea0
Oronno Akter Usha <ushaoronno1@gmail.com>
2025-01-07 23:06:27 +0600

    feat: add repeat property

--- Part 1/script.js
+++ Part 1/script.js
@@ -28,10 +28,21 @@
 //   duration: 2,
 //   delay: 1,
 // });
-gsap.from("h1", {
-  opacity: 0,
+
+// ----- stagger property -----
+// gsap.from("h1", {
+//   opacity: 0,
+//   duration: 2,
+//   y: 30,
+//   delay: 1,
+//   stagger: 1,
+// });
+
+// ----- repeat property ------
+gsap.to("#box", {
+  x: 1200,
   duration: 2,
-  y: 30,
   delay: 1,
-  stagger: 0.5,
+  rotate: 360,
+  repeat: 1,
 });
