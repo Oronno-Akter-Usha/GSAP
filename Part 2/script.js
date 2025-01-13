015e31da728adab0a2674dcf3e272b6d2304368c
Oronno Akter Usha <ushaoronno1@gmail.com>
2025-01-14 01:05:37 +0600

    feat: add  ScrollTrigger animations to h1 and h2

--- Part 2/script.js
+++ Part 2/script.js
@@ -4,10 +4,27 @@ gsap.from("#section1 .box", {
   duration: 2,
   rotate: 360,
 });
-gsap.from("#section2 .box", {
-  scale: 0,
-  delay: 1,
+
+gsap.from("#section2 h1", {
+  opacity: 0,
   duration: 2,
-  rotate: 360,
-  scrollTrigger: "#section2 .box",
+  x: 500,
+  scrollTrigger: {
+    trigger: "#section2 h1",
+    scroller: "body",
+    markers: true,
+    start: "top 50%",
+  },
+});
+
+gsap.from("#section2 h2", {
+  opacity: 0,
+  duration: 2,
+  x: -500,
+  scrollTrigger: {
+    trigger: "#section2 h2",
+    scroller: "body",
+    markers: true,
+    start: "top 50%",
+  },
 });
