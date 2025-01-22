d2e60d97755efa58ba30dbc96209556e11f476ef
Oronno Akter Usha <ushaoronno1@gmail.com>
2025-01-22 23:51:17 +0600

    add: image and cursor animations

--- 4.Create Cursor Animation/script.js
+++ 4.Create Cursor Animation/script.js
@@ -1,11 +1,27 @@
 var main = document.querySelector("#main");
 var cursor = document.querySelector("#cursor");
+var iamgeDiv = document.querySelector("#image");
 
 main.addEventListener("mousemove", function (dets) {
   gsap.to(cursor, {
     x: dets.x,
     y: dets.y,
-    duration: 1,
-    ease: "back.out",
+    duration: 0.6,
+  });
+});
+
+iamgeDiv.addEventListener("mouseenter", function () {
+  cursor.innerHTML = "View More";
+  gsap.to(cursor, {
+    scale: 4,
+    backgroundColor: "#ffffff9a",
+  });
+});
+
+iamgeDiv.addEventListener("mouseleave", function () {
+  cursor.innerHTML = "";
+  gsap.to(cursor, {
+    scale: 1,
+    backgroundColor: "#fff",
   });
 });
